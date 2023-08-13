12124930aeaf7ce3ad1cbfa6a929dedee22f8b7b
probitypic <probitypic@gmail.com>
2023-08-13 17:18:41 +0530

    climy app startup

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:climy/Services/location.dart';
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          constraints: BoxConstraints.expand(),
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("images/background.jpeg"), fit: BoxFit.cover),
+          ),
+        ),
+      ),
+    );
+  }
+}
